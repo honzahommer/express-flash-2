ea535f667f91b1983b416ca90f0323dfd70be973
jack2gs <sailor.g@163.com>
2017-01-13 20:36:49 +0800

    examples for express 4

--- examples/express4/app.js
+++ examples/express4/app.js
@@ -2,31 +2,35 @@ var express = require('express')
   , flash = require('../..')
   , util = require('util');
 
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var app = express();
 
 // configure Express
-app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
-  app.use(express.logger());
-  app.use(express.cookieParser('keyboard cat'));
-  app.use(express.session({ key: 'sid', cookie: { maxAge: 60000 }}));
+  app.use(logger('dev'));
+  app.use(cookieParser('keyboard cat'));
+  app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized:true}));
   
   // Use connect-flash middleware.  This will add a `req.flash()` function to
   // all requests, matching the functionality offered in Express 2.x.
   
   app.use(flash());
-  app.use(app.router);
-});
-
 
 app.get('/', function(req, res){
   res.render('index');
 });
 
 app.get('/flash', function(req, res){
-  req.flash('info', 'Hi there!')
+  res.flash('info', 'Hi there!')
   res.redirect('/');
 });
 
@@ -35,7 +39,7 @@ app.get('/no-flash', function(req, res){
 });
 
 app.get('/multiple-flash', function(req, res){
-    req.flash('info', ['Welcome', 'Please Enjoy']);
+    res.flash('info', ['Welcome', 'Please Enjoy']);
     res.redirect('/');
 });
 
